ebfeab41abf0c418aeeb70d35cbd2349ee3aaf2d
David Monagle <david.monagle@intrica.com.au>
2015-07-31 13:03:39 +1000

    Added bulk api

--- source/elasticsearch/api/actions/bulk.d
+++ source/elasticsearch/api/actions/bulk.d
@@ -2,3 +2,26 @@
 
 import elasticsearch.api.base;
 
+Response bulk(Client client, Parameters arguments = Parameters()) {
+	arguments.enforceParameter("body");
+
+	auto requestBody = arguments["body"];
+	auto params = arguments.validateAndExtract(
+		"consistency", "refresh", "replication", "type", "timeout"
+		);
+	string[] path;
+	if ("index" in arguments) {
+		path ~= arguments["index"];
+		if ("type" in arguments) {
+			path ~= arguments["type"];
+		}
+	}
+	path ~= "_bulk";
+
+	return client.performRequest(RequestMethod.POST, esPathify(path), params, requestBody);
+}
+
+Response bulk(Client client, string bulkBody, Parameters params = Parameters()) {
+	params["body"] = bulkBody;
+	return bulk(client, params);
+}
