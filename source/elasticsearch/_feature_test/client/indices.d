6feff896309d63265f2faf48812d7bdea1fcc71f
David Monagle <david.monagle@intrica.com.au>
2015-10-21 10:29:51 +1100

    Begin feature test

---
+++ source/elasticsearch/_feature_test/client/indices.d
@@ -0,0 +1,14 @@
+﻿module elasticsearch._feature_test.client.indices;
+
+debug (featureTest) {
+	import feature_test;
+	import elasticsearch._feature_test.elasticsearch_feature;
+
+	unittest {
+		feature!ElasticsearchFeature("Test Feature", "", (f) {
+				f.scenario("Test Scenario", {
+						featureTestPending;
+					});
+			}, "localES");
+	}
+}
---
+++ source/elasticsearch/_feature_test/elasticsearch_feature.d
@@ -0,0 +1,8 @@
+﻿module elasticsearch._feature_test.elasticsearch_feature;
+
+import feature_test;
+
+debug (featureTest) {
+	class ElasticsearchFeature : FeatureTest {
+	}
+}
